97e26d6e0d43d57758bc4b18549e69cd26c0c929
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-14 03:08:51 +0000

    refactor(db): add connection to mongodb

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log(`🔌 Connected to the MongoDB host : ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Oops.. can not connect to MongoDB :", err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
