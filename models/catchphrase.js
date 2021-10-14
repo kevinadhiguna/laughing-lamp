5cb33eb0b1dbd2758c3869989aa49380ab224515
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-14 03:08:30 +0000

    refactor(model): add a catchphrase model

---
+++ models/catchphrase.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const catchphraseSchema = new Schema({
+    movieName: {
+        type: String,
+    },
+    catchphrase: {
+        type: String,
+    },
+    movieContext: {
+        type: String,
+    },
+});
+
+const Catchphrase = mongoose.model('Catchphrase', catchphraseSchema);
+
+module.exports = Catchphrase;
