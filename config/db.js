c5eb4abf916249ad25115e0a695f4ebe59af204b
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-10-14 09:20:53 +0000

    fix: rename a variable and comment useFindAndModify

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
+            // useFindAndModify: false,
         });
         console.log(`🔌 Connected to the MongoDB host : ${conn.connection.host}`);
-    } catch (error) {
+    } catch (err) {
         console.error("Oops.. can not connect to MongoDB :", err);
         process.exit(1);
     }
