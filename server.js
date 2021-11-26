657ff5dddd739d6171ff47db0e5e75631b0a595c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-11-26 02:21:21 +0000

    refactor: add how to properly exit node-express server

--- server.js
+++ server.js
@@ -40,6 +40,16 @@ app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 const PORT = process.env.PORT || 5000;
 
 // Start server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`🚀 Server has been launched on port ${PORT}`);
+});
+
+// Exit Node-express server based on https://stackoverflow.com/a/21739334
+process.on('SIGTERM', () => {
+    console.info("SIGTERM received");
+    console.log("Closing HTTP server...");
+    server.close(err => {
+        console.log("HTTP server closed");
+        process.exit(err ? 1 : 0);
+    });
 });
