caaa9c696357ec89346481ecf5b1de8b1af1b0e7
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-03 07:32:14 +0000

    style: format code using prettier

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
-
-// For MongoDB (database) configuration
-const connectDb = require("./config/db");
-
-// Import routes
-const { catchphrases } = require("./routes/index");
+const app = express();
 
 // Swagger
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-const app = express();
-
 // Connect to MongoDB
+const connectDb = require("./config/db");
 connectDb();
 
 // parse requests of content-type : application/json
@@ -20,36 +14,36 @@ app.use(express.json());
 
 // Swagger configuration
 const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: "Catchphrases REST API",
-            description:
-                "A REST API powered by ExpressJS and MongoDB. This API provides movie catchphrases and the context of the catchprase in the movie.",
-        },
+  swaggerDefinition: {
+    info: {
+      title: "Catchphrases REST API",
+      description:
+      "A REST API powered by ExpressJS and MongoDB. This API provides movie catchphrases and the context of the catchprase in the movie.",
     },
-    apis: ["./routes/catchphrases.js"],
+  },
+  apis: ["./routes/catchphrases.js"],
 };
-
-// routes
-app.use("/catchphrases", catchphrases);
-
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Register route
+const { catchphrases } = require("./routes/index");
+app.use("/catchphrases", catchphrases);
 
 // port configuration
 const PORT = process.env.PORT || 5000;
 
 // Start server
 const server = app.listen(PORT, () => {
-    console.log(`🚀 Server has been launched on port ${PORT}`);
+  console.log(`🚀 Server has been launched on port ${PORT}`);
 });
 
 // Exit Node-express server based on https://stackoverflow.com/a/21739334
-process.on('SIGTERM', () => {
-    console.info("SIGTERM received");
-    console.log("Closing HTTP server...");
-    server.close(err => {
-        console.log("HTTP server closed");
-        process.exit(err ? 1 : 0);
-    });
+process.on("SIGTERM", () => {
+  console.info("SIGTERM received");
+  console.log("Closing HTTP server...");
+  server.close((err) => {
+    console.log("HTTP server closed");
+    process.exit(err ? 1 : 0);
+  });
 });
