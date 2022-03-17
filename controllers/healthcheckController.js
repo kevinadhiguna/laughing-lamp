6568ebc1004d9ff6f0b9eb148f229c8d8823b0fd
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-17 04:27:30 +0000

    refactor: format time

--- controllers/healthcheckController.js
+++ controllers/healthcheckController.js
@@ -1,8 +1,12 @@
 const isServiceHealthy = (req, res, next) => {
+  // Format time (reference : https://www.freecodecamp.org/news/javascript-date-now-how-to-get-the-current-date-in-javascript/)
+  const timeElapsed = Date.now();
+  const today = new Date(timeElapsed);
+
   const healthyData = {
     uptimeDuration: process.uptime(),
     message: "Service is healthy !",
-    time: Date.now().toLocaleString(),
+    time: today.toUTCString(), // "Thu, 11 May 2021 09:23:41 GMT"
   };
 
   const unhealthyData = {
