aa31447ad2fdb0e4ce2f3f260114f5cba2ec43bf
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-03-17 05:24:28 +0000

    refactor: handle graceful shutdown using packgae

--- server.js
+++ server.js
@@ -45,20 +45,43 @@ const server = app.listen(PORT, HOSTNAME, () => {
   );
 });
 
+// == Graceful shutdown (handling signals such as SIGINT and SIGTERM) using 'lil-http-terminator' package ==
+
+const httpTerminator = require("lil-http-terminator")({ server });
+
+const shutdown = async (signal) => {
+  console.log(`\nReceived ${signal}, shutting down..`);
+  const { code, message, success, error } = await httpTerminator.terminate();
+  console.log(
+    `HTTP server closure result :
+     Success : ${success}
+     Code : ${code}
+     Message : ${message}
+     Error (if exists) : ${error || ""}`
+  );
+  process.exit(error ? 1 : 0);
+  // process.exit(0);
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+// == Manually handle graceful suhtdown (such as when receiving SIGINT and SIGTERM) ==
+
 // Handle interrupt signal (SIGINT)
-process.on("SIGINT", () => {
-  console.log("Received SIGINT, process interrupted...");
-  server.close((errSigint) => {
-    console.log("Closing HTTP server after receiving SIGINT..");
-    process.exit(errSigint ? 1 : 0);
-  });
-});
+// process.on("SIGINT", () => {
+//   console.log("Received SIGINT, process interrupted...");
+//   server.close((errSigint) => {
+//     console.log("Closing HTTP server after receiving SIGINT..");
+//     process.exit(errSigint ? 1 : 0);
+//   });
+// });
 
 // Exit Node-express server based on https://stackoverflow.com/a/21739334
-process.on("SIGTERM", () => {
-  console.log("Received SIGTERM, process terminated...");
-  server.close((errSigterm) => {
-    console.log("Closing HTTP server after receiving SIGTERM..");
-    process.exit(errSigterm ? 1 : 0);
-  });
-});
+// process.on("SIGTERM", () => {
+//   console.log("Received SIGTERM, process terminated...");
+//   server.close((errSigterm) => {
+//     console.log("Closing HTTP server after receiving SIGTERM..");
+//     process.exit(errSigterm ? 1 : 0);
+//   });
+// });
